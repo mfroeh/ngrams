a67f4ab976bdb7c44e463e4e4b977db8cc8a30be
mfroeh <mfroeh0@pm.me>
2025-03-09 12:30:52 +0100

    add modes

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,73 @@
-use std::{collections::HashMap, fs, io, ops::Add, path::PathBuf};
+use std::{collections::HashMap, fs, io, path::PathBuf};
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
-    #[arg(short, long)]
-    path: PathBuf,
+    path: Vec<PathBuf>,
 
     #[arg(short, long, default_value_t = 2)]
     n: usize,
 
     #[arg(short, long)]
-    symbols_only: bool,
+    mode: Mode,
+}
+
+#[derive(ValueEnum, Debug, Clone)]
+enum Mode {
+    Alpha,
+    Numeric,
+    Alnum,
+    Symbols,
+    All,
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let walk = ignore::WalkBuilder::new(&args.path).build();
+    let mut walk = ignore::WalkBuilder::new(&args.path[0]);
+    for path in args.path.iter().skip(1) {
+        walk.add(path);
+    }
+    let walk = walk.build();
 
     let mut ngrams: HashMap<String, u64> = HashMap::new();
     for f in walk
         .filter_map(|e| e.ok())
         .filter(|e| e.metadata().is_ok_and(|m| !m.is_dir()))
     {
-        let content = fs::read_to_string(f.path())?;
+        let content = fs::read_to_string(f.path());
+        if let Err(e) = content {
+            eprintln!("{}: {}", f.path().to_str().unwrap(), e);
+            continue;
+        }
+        let content = content.unwrap();
+
         for seq in content.split_whitespace() {
             for ngram in seq.chars().collect::<Vec<_>>().windows(args.n) {
-                if args.symbols_only && ngram.iter().any(|c| c.is_ascii_alphabetic()) {
-                    continue;
+                match args.mode {
+                    Mode::Alpha => {
+                        if ngram.iter().any(|c| !c.is_alphabetic()) {
+                            continue;
+                        }
+                    }
+                    Mode::Numeric => {
+                        if ngram.iter().any(|c| !c.is_numeric()) {
+                            continue;
+                        }
+                    }
+                    Mode::Alnum => {
+                        if ngram.iter().any(|c| !c.is_alphanumeric()) {
+                            continue;
+                        }
+                    }
+                    Mode::Symbols => {
+                        if ngram.iter().any(|c| c.is_alphanumeric()) {
+                            continue;
+                        }
+                    }
+                    Mode::All => {}
                 }
 
                 let str = String::from_iter(ngram);
@@ -38,13 +76,16 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    let mut csv = csv::Writer::from_writer(io::stdout());
     let mut entries: Vec<(String, u64)> = ngrams.into_iter().collect();
     entries.sort_by(|a, b| a.1.cmp(&b.1));
 
+    let mut csv = csv::WriterBuilder::new()
+        .quote(b'\'')
+        .from_writer(io::stdout());
     for (ngram, count) in entries.into_iter().rev() {
         csv.write_record(&[ngram, count.to_string()])?;
     }
+    csv.flush()?;
 
     Ok(())
 }
